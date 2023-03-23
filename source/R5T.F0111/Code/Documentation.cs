21dcb965f6a162256a0448fa6ee50d913d3d5183
davidcoats <david@getsafetycone.com>
2023-03-23 02:29:08 -0700

    Interim changes.

--- source/R5T.F0111/Code/Documentation.cs
+++ source/R5T.F0111/Code/Documentation.cs
@@ -8,5 +8,9 @@ namespace R5T.F0111
 	/// </summary>
 	public static class Documentation
 	{
-	}
+        /// <summary>
+        /// Find project files by searching repository directories on the local machine.
+        /// </summary>
+        public static readonly object Find_AllProjectFilePaths;
+    }
 }
---
+++ source/R5T.F0111/Code/Functionality/IOperations.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Linq;
+
+using R5T.T0132;
+using R5T.T0159;
+using R5T.T0159.Extensions;
+
+
+namespace R5T.F0111
+{
+    [FunctionalityMarker]
+    public partial interface IOperations : IFunctionalityMarker
+    {
+        /// <summary>
+        /// <inheritdoc cref="Documentation.Find_AllProjectFilePaths" path="/summary"/>
+        /// </summary>
+        public string[] Find_AllProjectFilePaths(
+            ITextOutput textOutput)
+        {
+            // Output project paths to current run date's directory.
+            var repositoriesDirectoryPaths = Instances.RepositoriesDirectoryPaths.AllOfMine;
+
+            // Write out the repositories directories to the human output.
+            textOutput.HumanOutput.WriteLine("Projects will be found in directories:");
+            foreach (var repositoriesDirectoryPath in repositoriesDirectoryPaths)
+            {
+                textOutput.HumanOutput.WriteLine($"\t{repositoriesDirectoryPath}");
+            }
+
+            var projectFilePaths = Instances.FileSystemOperator.GetAllProjectFilePaths_FromRepositoriesDirectoryPaths(
+                repositoriesDirectoryPaths,
+                textOutput)
+                .OrderAlphabetically()
+                .Now();
+
+            return projectFilePaths;
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="Documentation.Find_AllProjectFilePaths" path="/summary"/>
+        /// Also removes all projects that should not be built.
+        /// Outputs the projects to a text file.
+        /// </summary>
+        /// <returns>
+        /// The filtered list of projects.
+        /// </returns>
+        public string[] Find_AllProjectFilePaths(
+            string allProjectsListTextFilePath,
+            string projectsOfInterestTextFilePath,
+            string doNotBuildProjectsListTextFilePath,
+            ITextOutput textOutput)
+        {
+            textOutput.WriteInformation("Searching for all project files...");
+
+            var allProjectFilePaths = this.Find_AllProjectFilePaths(
+                textOutput);
+
+            textOutput.WriteInformation("Found {ProjectFileCount} project files.", allProjectFilePaths.Length);
+
+            textOutput.WriteSectionSeparator();
+
+            // Write out all projects.
+            Instances.FileOperator.WriteLines_Synchronous(
+                allProjectsListTextFilePath,
+                allProjectFilePaths);
+
+            textOutput.WriteInformation("Wrote all project files to:\n\t{0}.", allProjectsListTextFilePath);
+
+            // Now filter out projects we don't want to built.
+            textOutput.WriteInformation("Filtering out project files that should not be built...");
+
+            textOutput.WriteInformation("Using do-not-build project files list from:\n\t{0}.", doNotBuildProjectsListTextFilePath);
+
+            var projectFilePathsToSkip = Instances.FileOperator.ReadAllLines_Synchronous(
+               doNotBuildProjectsListTextFilePath);
+
+            var projectFilePaths = allProjectFilePaths
+                .Except(projectFilePathsToSkip)
+                .Now();
+
+            textOutput.WriteInformation("After filtering {0} project files remain.", projectFilePaths.Length);
+
+            // Write out projects list we actually want to build.
+            Instances.FileOperator.WriteLines_Synchronous(
+                projectsOfInterestTextFilePath,
+                projectFilePaths);
+
+            textOutput.WriteInformation("Wrote project files to:\n\t{0}.", projectsOfInterestTextFilePath);
+
+            return projectFilePaths;
+        }
+    }
+}
